aa898895b5fc599ceb99654def087bccb84d8f68
Dmit Bashmakov <denegniy1@gmail.com>
2024-10-23 17:21:21 +0500

    added CRUD via ViewSet for courses

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
     path('study/', include('study.urls', namespace='study')),
+    path('users/', include('users.urls', namespace='users')),
 ]
--- study/urls.py
+++ study/urls.py
@@ -1,7 +1,11 @@
+from os import path
+from rest_framework.routers import DefaultRouter
 from study.apps import StudyConfig
+from study.views import CourseViewSet
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='courses')
 
 app_name = StudyConfig.name
 
-urlpatterns = [
-
-]
+urlpatterns = [] + router.urls
