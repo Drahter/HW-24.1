0957a93bff5f3d054fc81c6c58edc25ce9b7138b
Drahter <Denegniy1@gmail.com>
2024-11-10 22:51:37 +0500

    added Createview for users, JWT autorisation

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payment-list'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
 
-from users.models import Payment
-from users.serializsers import PaymentSerializer
+from users.models import Payment, User
+from users.serializsers import PaymentSerializer, UserSerializer
 
 
 # Create your views here.
@@ -16,3 +16,13 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['payment_method', 'course', 'lesson']
     ordering_fields = ['date']
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
