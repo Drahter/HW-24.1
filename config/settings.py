34d519cffd5e4bc435078bdac7450473f67002dd
Drahter <Denegniy1@gmail.com>
2024-11-10 23:02:19 +0500

    all controllers but login and refresh locked by authorisation

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,12 @@ LOGOUT_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializsers import PaymentSerializer, UserSerializer
@@ -21,6 +22,7 @@ class PaymentListAPIView(generics.ListAPIView):
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
