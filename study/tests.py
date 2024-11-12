e74322b92a0cc5c3ef481ce5de4c591388a9b036
Dmit Bashmakov <denegniy1@gmail.com>
2024-11-12 14:00:49 +0500

    added test for RETRIEVE lesson

--- study/tests.py
+++ study/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from study.models import Course, Lesson
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='test@example.com')
+        self.course = Course.objects.create(title='Тестовый курс', description='Описание')
+        self.lesson = Lesson.objects.create(title='Тестовый урок', description='Описание урока', course=self.course,
+                                            owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse('study:lesson-get', args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
